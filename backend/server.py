e6a008b7dcf06f31678a419d585a53ed370b1cc5
Pinki Singh <web.pinkisingh@gmail.com>
2026-02-07 22:03:35 +0530

    added fastapi cors middleware

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 from textblob import TextBlob
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app = FastAPI()
 
